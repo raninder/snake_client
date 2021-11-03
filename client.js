e24da1218c4cfea1755a94d3a1778d8825668825
raninder <raninderdhillon@gmail.com>
2021-11-03 22:11:48 +0000

    export

--- client.js
+++ client.js
@@ -1,8 +1,23 @@
 const net = require("net");
 
-const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541// PORT number here,
-});
-
-module.exports = conn;
+const connect = function () {
+    console.log("inside connect");
+      const conn = net.createConnection({
+        host: "165.227.47.243",
+        port: 50541// PORT number here,
+      });
+      conn.on("connect", ()=>{
+          console.log("connected");
+          conn.write("Name: RKD");
+      });
+      conn.on('data', function(data) {
+        console.log('Received: ' + data);
+    });
+    
+      // interpret incoming data as text
+      conn.setEncoding("utf8");
+    
+      return conn;
+    };
+    
+module.exports = {connect};
--- play1.js
+++ play1.js
@@ -1,12 +1,4 @@
 const conn = require("./client");
-
-conn.on("connect", () => {
-    console.log("connected");
-})
-conn.on('data', function (data) {
-    console.log('Received: ' + data);
-});
-// interpret incoming data as text
-conn.setEncoding("utf8");
-
+console.log(conn.connect);
+conn.connect();
 console.log("Connecting ...");
